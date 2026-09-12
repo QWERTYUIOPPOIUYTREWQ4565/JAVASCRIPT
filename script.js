35709599ddeaac034e1a40264bdaab0f34e9cce3
QWERTYUIOPPOIUYTREWQ4565 <rk3313733@gmail.com>
2026-09-12 17:28:44 +0530

    Event bubbling(code)

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let ul = document.querySelector("ul");
+let a  = document.querySelector(".a")
+let b  = document.querySelector(".b")
+let c  = document.querySelector(".c")
+let btn = document.querySelector("button");
 
-ul.addEventListener("click" , function(dets){
-    dets.target.classList.toggle("lt");
+btn.addEventListener("click",function(){
+    console.log("button clicked");
+});
+
+c.addEventListener("click",function(){
+    console.log("c clicked");
+});
+
+b.addEventListener("click",function(){
+    console.log("b clicked");
+});
+
+a.addEventListener("click",function(){
+    console.log("a clicked");
 });
