5394db411ac9fdddaf4b60d162fe997ce26bbc3d
QWERTYUIOPPOIUYTREWQ4565 <rk3313733@gmail.com>
2026-09-13 15:42:19 +0530

    LocalStorage

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-let count = 0;
-let seconds = 5;
-let progress = document.querySelector(".progress-bar");
-let percentText = document.querySelector("#percentText");
-let heading = document.querySelector("h2");
+//localStorage
+// store kaise kare - setItem
+// data fetch kaise kare - getItem
+// remove kaise kare - removeItem
+// update kaise kare - setItem only updates 
 
-let download = setInterval(function () {
-    if (count < 100) {
-        count++;
-        progress.style.width = `${count}%`;
-        percentText.textContent = `${count}%`;
-    } else {
-       document.querySelector("h1").textContent = "Downloaded."
-        clearInterval(download);
-    }
-}, (seconds * 1000) / 100);
+let val = localStorage.getItem("name");
