47402d790fa791014c1803f35d927e5fc504163d
QWERTYUIOPPOIUYTREWQ4565 <rk3313733@gmail.com>
2026-09-13 13:43:21 +0530

    Form Validation Code

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-let nm = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function(dets){
+form.addEventListener("submit", function(dets) {
+
     dets.preventDefault();
-   let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   let ans = emailRegex.test("harsh@test.com");
-   console.log(ans);
-})
+
+    document.querySelector("#emailError").textContent = "";
+    document.querySelector("#passwordError").textContent = "";
+    document.querySelector("#resultMessage").textContent = "";
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+    let emailans = emailRegex.test(email.value);
+
+    let passwordans = passwordRegex.test(password.value);
+
+    let isValid = true;
+
+    if (!emailans) {
+
+        document.querySelector("#emailError").textContent = "Email is incorrect";
+
+        document.querySelector("#emailError").style.display = "initial";
+
+        isValid = false;
+    }
+
+    if (!passwordans) {
+
+        document.querySelector("#passwordError").textContent = "Password is incorrect";
+
+        document.querySelector("#passwordError").style.display = "initial";
+
+        isValid = false;
+    }
+
+    if (isValid) {
+
+        document.querySelector("#resultMessage").textContent = "Everything is correct";
+    }
+
+});
