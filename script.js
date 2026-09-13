2ffbb2922cab4f47225960156da8d02e247b1581
QWERTYUIOPPOIUYTREWQ4565 <rk3313733@gmail.com>
2026-09-13 22:46:46 +0530

    Theme Toggler

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-//localStorage
-// store kaise kare - setItem
-// data fetch kaise kare - getItem
-// remove kaise kare - removeItem
-// update kaise kare - setItem only updates 
+function setDarkOrLight() {
 
-let val = localStorage.getItem("name");
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+
+    } else {
+
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+}
+
+if(localStorage.getItem("theme")){
+    document.body.classList.add(localStorage.getItem("theme"));
+}
+else{
+    setDarkOrLight();
+}
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", function() {
+        if(!localStorage.getItem("theme")){
+            setDarkOrLight()
+        }
+    });
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    if(document.body.classList.contains("dark")){
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+
+        localStorage.setItem("theme","light");
+    }else{
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+
+        localStorage.setItem("theme","dark");
+    }
+});
+
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", function() {
+
+        setDarkOrLight();
+
+    });
+
+document.body.classList.add(localStorage.getItem("theme"));
