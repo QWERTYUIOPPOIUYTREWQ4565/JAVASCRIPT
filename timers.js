be694ff3b6bb57a11b991ec19b02590d636c4f33
rishi_kumar_10 <rk3313733@gmail.com>
2026-09-20 13:16:53 +0530

    Add files via upload
    Timers and eventListeners(Memory Leaks)

---
+++ timers.js
@@ -0,0 +1,10 @@
+let count = 0;
+const int = setInterval(() =>{
+    if(count < 10){
+        count++;
+        console.log(count);
+    }else{
+        clearInterval(int);
+        console.log("finished");
+    }
+},500);
